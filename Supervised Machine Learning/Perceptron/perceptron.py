0b8d1294125100162a78c06efe52f71bdea5d850
Kyle Sanderfer <kjs15@rice.edu>
2025-05-05 15:41:03 -0500

    perceptron done

--- Supervised Machine Learning/Perceptron/perceptron.py
+++ Supervised Machine Learning/Perceptron/perceptron.py
@@ -6,22 +6,20 @@ class Perceptron(object):
         self.epochs = epochs
         
     def train(self, X, y):
-        self.w_ = np.random.rand(1 + X.shape[1])
-        
-        self.errors_ = []
-        
+        self.w_ = np.random.rand(1 + X.shape[1])  # Initialize weights randomly
+        self.errors_ = []  # Track errors for each epoch
+
         for _ in range(self.epochs):
             errors = 0
             for xi, target in zip(X, y):
-                update = self.eta * (self.predict(xi) - target)
-                self.w_[:-1] -= update*xi
-                self.w_[-1] -= update
-                errors += int(update != 0)
-            if errors == 0:
-                return self
-            else:
-                self.errors_.append(errors)
-            
+                update = self.eta * (target - self.predict(xi))  # Correct update rule
+                self.w_[:-1] += update * xi  # Update weights
+                self.w_[-1] += update  # Update bias
+                errors += int(update != 0)  # Count misclassifications
+            self.errors_.append(errors)  # Append errors for this epoch
+            if errors == 0:  # Stop early if no errors
+                break
+
         return self
     
     def net_input(self, X):
