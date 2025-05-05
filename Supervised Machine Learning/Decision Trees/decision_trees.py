99d2c0572d96f89554b56fe1cd1584131060811b
Kyle Sanderfer <kjs15@rice.edu>
2025-05-05 17:26:35 -0500

    decision tree notebook

--- Supervised Machine Learning/Decision Trees/decision_trees.py
+++ Supervised Machine Learning/Decision Trees/decision_trees.py
@@ -1,14 +1,14 @@
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 class DecisionTree:
-    def __init__(self, max_depth, random_state, is_classifier = True):
+    def __init__(self, max_depth, is_classifier = True):
     
         self.is_classifier = is_classifier
 
         if self.is_classifier:
-            self.tree = DecisionTreeClassifier(max_depth=max_depth, random_state=random_state)
+            self.tree = DecisionTreeClassifier(max_depth=max_depth)
         else:
-            self.tree = DecisionTreeRegressor(max_depth=max_depth, random_state=random_state)
+            self.tree = DecisionTreeRegressor(max_depth=max_depth)
         
     def fit(self, X_train, y_train):
         self.tree.fit(X_train, y_train)
